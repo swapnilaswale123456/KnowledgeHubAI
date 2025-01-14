0cb893c9563cec5c41618219838910734b9aadbb
swapnilaswale123456 <swapnilaswale@hotmail.com>
2025-01-14 18:33:41 +0530

    first commit

---
+++ app/modules/blog/routes/api/BlogRoutes.Index.Api.tsx
@@ -0,0 +1,27 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { MetaTagsDto } from "~/application/dtos/seo/MetaTagsDto";
+import { verifyUserHasPermission } from "~/utils/helpers/.server/PermissionsService";
+import { BlogPostWithDetails, getAllBlogPosts } from "../../db/blog.db.server";
+import { getTenantIdOrNull } from "~/utils/services/.server/urlService";
+import { requireAuth } from "~/utils/loaders.middleware";
+
+export namespace BlogRoutesIndexApi {
+  export type LoaderData = {
+    metatags: MetaTagsDto;
+    items: BlogPostWithDetails[];
+  };
+
+  export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+    await requireAuth({ request, params });
+    const tenantId = await getTenantIdOrNull({ request, params });
+    if (tenantId === null) {
+      await verifyUserHasPermission(request, "admin.blog.view");
+    }
+    const items = await getAllBlogPosts({ tenantId });
+    const data: LoaderData = {
+      metatags: [{ title: `Blog | ${process.env.APP_NAME}` }],
+      items,
+    };
+    return json(data);
+  };
+}
