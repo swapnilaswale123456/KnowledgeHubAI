2cd044c84d89ec6756db4fcaa8899496520c09d9
swapnil <swapnilaswale@hotmail.com>
2025-05-01 15:54:33 +0530

    Addeed python url

--- app/services/research/researchRequests.server.ts
+++ app/services/research/researchRequests.server.ts
@@ -197,7 +197,7 @@ export class ResearchRequestsService {
   private baseUrl: string;
 
   private constructor() {
-    this.baseUrl = 'http://localhost:5000/api/v1';
+    this.baseUrl = 'https://reddit-researcher-1aaa93b8186d.herokuapp.com/api/v1';
   }
 
   public static getInstance(): ResearchRequestsService {
