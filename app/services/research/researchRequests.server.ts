98d95a2b84db7771f6378fb8c4b26123b3a3576c
swapnil <swapnilaswale@hotmail.com>
2025-05-02 00:36:04 +0530

    Added python env variable

--- app/services/research/researchRequests.server.ts
+++ app/services/research/researchRequests.server.ts
@@ -197,7 +197,7 @@ export class ResearchRequestsService {
   private baseUrl: string;
 
   private constructor() {
-    this.baseUrl = 'http://localhost:5000/api/v1';
+    this.baseUrl = process.env.PYTHON_API_ENDPOINT || 'http://localhost:5000/api/v1';
   }
   
   public static getInstance(): ResearchRequestsService {
